adb4e63e346960189010bb8ab24237e982343fa8
crisalm29 <cris29sk8@gmail.com>
2016-10-05 22:20:49 -0600

    Mapped to host

--- dondeEsHoy-frontEnd/www/js/services.js
+++ dondeEsHoy-frontEnd/www/js/services.js
@@ -4,7 +4,8 @@ angular.module('starter.services', [])
             var obtenerLocales = function (lat, long) {
                 var p = $http({
                     method: 'POST',
-                    url: "http://localhost:49986/googlePlaces",
+                    url: "http://crisalm29-001-site1.dtempurl.com/googlePlaces",
+                    //url: "http://localhost:49986/googlePlaces",
                     data: {
                         lat: lat,
                         lng: long
@@ -25,4 +26,30 @@ angular.module('starter.services', [])
                 obtenerLocales: obtenerLocales
             };
         })
-        
+//        .service('userServices', function ($http, $ionicLoading, $ionicPopup, $timeout) {
+//            
+//            var obtenerLocales = function (lat, long) {
+//                var p = $http({
+//                    method: 'POST',
+//                    url: "http://localhost:49986/googlePlaces",
+//                    data: {
+//                        lat: lat,
+//                        lng: long
+//                    }
+//
+//                });
+//                return p.success(function (data) {
+//                    $ionicLoading.hide();
+//                    return data;
+//                }).error(function (e) {
+//                    $ionicLoading.hide();
+//
+//                });
+//            };
+//
+//
+//            return {
+//                obtenerLocales: obtenerLocales
+//            };
+//        })
+//        
