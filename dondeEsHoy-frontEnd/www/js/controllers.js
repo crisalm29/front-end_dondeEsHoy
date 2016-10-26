851c2e25f6cc9c44d99ac6805a7809986624a31c
franvr94 <franvr94@gmail.com>
2016-10-26 14:13:15 -0600

    Corrección 2 actualizar usuario

--- dondeEsHoy-frontEnd/www/js/controllers.js
+++ dondeEsHoy-frontEnd/www/js/controllers.js
@@ -92,7 +92,7 @@ angular.module('starter.controllers', [])
                 });
             };
         })
-        .controller('ProfileCtrl', function ($scope, $ionicPopup, obtenerInfoPorEmail, $cordovaImagePicker) {
+        .controller('ProfileCtrl', function ($scope,$http ,$ionicPopup, obtenerInfoPorEmail, $cordovaImagePicker) {
             $scope.data = {};
             $scope.data.email = usuario;
             var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
@@ -135,6 +135,7 @@ angular.module('starter.controllers', [])
 
                     });
                     return p.success(function (data) {
+                    console.log(data);
                         if (data.result !== false) {
                             
                             var alertPopup = $ionicPopup.alert({
@@ -154,10 +155,10 @@ angular.module('starter.controllers', [])
         };
 
             $scope.actualizarUsuario = function () {
-                if ($scope.data.Opassword === $scope.data.oldpassword) {
+                if ($scope.data.Opassword === $scope.data.Oldpassword) {
                     if ($scope.data.Newpassword === $scope.data.Newpassword2) {
-                        if($scope.collection !== ""){
-                            $scope.data.imageBase64 = $scope.collection;                            
+                        if($scope.collection.selectedImage !== ""){
+                            $scope.data.imageBase64 = $scope.collection.selectedImage;                            
                         }
                         codificarBase64($scope.data.imageBase64);
                         servicioActualizar();
@@ -201,6 +202,7 @@ angular.module('starter.controllers', [])
                         window.plugins.Base64.encodeFile($scope.collection.selectedImage, function (base64) {  // Encode URI to Base64 needed for contacts plugin
                             $scope.collection.selectedImage = base64;
                             $scope.addContact();    // Save contact
+                            $scope.data.imageBase64 = $scope.collection.selectedImage;
                         });
                     }
                 }, function (error) {
